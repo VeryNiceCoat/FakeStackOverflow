c8d96dfdaa9eff702ba85dee28a543c61241bc93
Mayukh-Banik <mayukh.banik@stonybrook.edu>
2023-11-29 13:09:56 -0500

    Building Login.js
    Building Login.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,34 @@
 // ************** DEFINE YOUR REACT COMPONENTS in ./components directory **************
 import './stylesheets/App.css';
 import FakeStackOverflow from './components/fakestackoverflow.js'
+import Login from './components/login.js'
+import { useState } from 'react';
 
 function App() {
+  const [showWelcomePage, setShowWelcomePage] = useState(true);
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showRegisterForm, setShowRegisterForm] = useState(false);
+  const [showRejectedMessage, setShowRejectedMessage] = useState(false);
+  const [showHomePage, setShowHomePage] = useState(false);
+  const [identifier, setIdentifier] = useState(undefined);
+
   return (
     <section className="fakeso">
-      <FakeStackOverflow />
+      {showWelcomePage && (
+        <Login
+          showWelcomePage={showWelcomePage}
+          setShowWelcomePage={setShowWelcomePage}
+          showLoginForm={showLoginForm}
+          setShowLoginForm={setShowLoginForm}
+          showRegisterForm={showRegisterForm}
+          setShowRegisterForm={setShowRegisterForm}
+          showRejectedMessage={showRejectedMessage}
+          setShowRejectedMessage={setShowRejectedMessage}
+          showHomePage={showHomePage}
+          setShowHomePage={setShowHomePage}
+        />
+      )}
+      {showHomePage && <FakeStackOverflow />}
     </section>
   );
 }
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,26 +1,60 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 
-function Login() {
-    const [showLoginForm, setShowLoginForm] = useState(false);
-    const [showRegisterForm, setShowRegisterForm] = userState(false);
-    const [showRejectedMessage, setShowRejectedMessage] = useState(false);
+function Login (props) {
+    const {
+        showWelcomePage,
+        setShowWelcomePage,
+        showLoginForm,
+        setShowLoginForm,
+        setShowRegisterForm,
+        showRejectedMessage,
+        setShowRejectedMessage,
+        showHomePage,
+        setShowHomePage
+    } = props;
 
     const handleLoginClick = () => {
-        console.log('login click detected');
+        setShowRegisterForm(false);
+        setShowRejectedMessage(false);
         setShowLoginForm(true);
     }
+
+    const handleRegisterClick = () => {
+        setShowLoginForm(false);
+        setShowRejectedMessage(false);
+        setShowRegisterForm(true);
+    }
+
+    const handleLoginSubmit = event => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const username = formData.get('name');
+        const password = formData.get('password');
+        console.log('Username:', username, 'Password:', password);
+
+
+    };
 
     return(
         <div className='loginContainer'>
             <h3>Welcome to FakeStackOverflow</h3>
             <button onClick={handleLoginClick}>Login</button>
-            <button>Register</button>
+            <button onClick={handleRegisterClick}>Register</button>
             <button>Guest</button>
             {showLoginForm && (
+                <form onSubmit={handleLoginSubmit}>
+                    <input type='text' name='name' placeholder='Username'/>
+                    <input type='text' name='password' placeholder='Password'/>
+                    <button type='submit'>Submit</button>
+                </form>
+            )}
+            {setShowRegisterForm && (
                 <form>
-                    <input type='text' placeholder='Username'/>
-                    <input type='text' placeholder='Password'/>
+                    <input type='text' name='name' placeholder='Username'/>
+                    <input type='text' name='email' placeholder='Email'/>
+                    <input type='text' name='password' placeholder='Password'/>
                     <button type='submit'>Submit</button>
                 </form>
             )}
