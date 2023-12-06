c0ab1374702234e1d50f8ed141d9868bbaa42f2d
Mayukh-Banik <mayukh.banik@stonybrook.edu>
2023-12-06 14:25:00 -0500

    Guest Account Button now doesn't work
    Made Guest Profile Broken Throw an Error

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,10 +1,54 @@
 import React, { useState } from 'react'
+import Axios from 'axios'
+
 const Sidebar = props => {
   const [selected, setSelected] = useState('Questions')
+
+  // Handlers for different link clicks
+  const handleQuestionsClick = () => {
+    handleLinkClick('Questions')
+    if (props.onQuestionsClick) {
+      setSelected('Questions')
+      props.onQuestionsClick()
+    }
+  }
+
+  const handleTagsClick = () => {
+    handleLinkClick('Tags')
+    if (props.onTagsPageClick) {
+      setSelected('Tags')
+      props.onTagsPageClick()
+    }
+  }
+
+  const handleProfileClick = async () => {
+    try {
+      const response = await Axios.get(
+        'http://localhost:8000/users/accountType',
+        {
+          withCredentials: true
+        }
+      )
+      if (response.data === 1) {
+        throw new Error("You're a guest, you don't have a profile")
+      }
+    } catch (error) {
+      window.alert(error.message)
+      return
+    }
+    handleLinkClick('Profile')
+    if (props.onProfilePageClick) {
+      setSelected('Profile')
+      props.onProfilePageClick()
+    }
+  }
+
+  // Common function to handle link click
   const handleLinkClick = linkName => {
     props.onSelect(linkName)
   }
 
+  // Styling function
   const linkStyle = linkName => {
     return {
       backgroundColor: selected === linkName ? 'gray' : 'paleturquoise'
@@ -18,43 +62,21 @@ const Sidebar = props => {
           <button
             id='Questions'
             style={linkStyle('Questions')}
-            onClick={e => {
-              e.preventDefault()
-              handleLinkClick('Questions')
-              if (props.onQuestionsClick) {
-                setSelected('Questions')
-                props.onQuestionsClick()
-              }
-            }}
+            onClick={handleQuestionsClick}
           >
             Questions
           </button>
         </li>
         <li>
-          <button
-            id='Tags'
-            style={linkStyle('Tags')}
-            onClick={() => {
-              handleLinkClick('Tags')
-              if (props.onTagsPageClick) {
-                setSelected('Tags')
-                props.onTagsPageClick()
-              }
-            }}
-          >
+          <button id='Tags' style={linkStyle('Tags')} onClick={handleTagsClick}>
             Tags
           </button>
         </li>
         <li>
-          <button id="Profile"
+          <button
+            id='Profile'
             style={linkStyle('Profile')}
-            onClick={() => {
-              handleLinkClick('Profile')
-              if (props.onProfilePageClick) {
-                setSelected('Profile')
-                props.onProfilePageClick()
-              }
-            }}
+            onClick={handleProfileClick}
           >
             Your Profile
           </button>
